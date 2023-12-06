9d165b173056fe44cc7753630df7fa8813ab5077
Dani Guardiola <hi@daniguardio.la>
2023-12-06 23:37:16 +0100

    dark theme by default

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -3,10 +3,14 @@ import { createSignal } from "solid-js";
 export function isDarkTheme() {
   if (typeof window === "undefined") return false;
   if (localStorage.theme === "dark") return true;
-  return (
-    !("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-  );
+  // sadly, there is no way to do "dark mode by default,
+  // light theme if the user wants it" because the default for
+  // "prefers-color-scheme" is "light" and "no-preference" was
+  // removed from the spec, so this is the only way ¯\_(ツ)_/¯
+  // return (
+  //   !("theme" in localStorage) &&
+  //   window.matchMedia("(prefers-color-scheme: dark)").matches
+  return !("theme" in localStorage);
 }
 
 export function ThemeScript() {
