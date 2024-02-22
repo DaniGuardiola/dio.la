3630078e957d1398d8d3d38a84d589afd6ecb3e4
Dani Guardiola <hi@daniguardio.la>
2024-02-22 19:34:40 +0100

    publish post

--- src/components/ArticleSeriesIndex/TheOpenClosedComponentIndex.tsx
+++ src/components/ArticleSeriesIndex/TheOpenClosedComponentIndex.tsx
@@ -6,9 +6,9 @@ export function TheOpenClosedComponentIndex() {
       name="The open/closed component"
       articleIds={[
         "the-open-closed-component-part-1",
-        "the-open-closed-component-part-2",
-        "the-open-closed-component-part-3",
-        "the-open-closed-component-part-4",
+        // "the-open-closed-component-part-2",
+        // "the-open-closed-component-part-3",
+        // "the-open-closed-component-part-4",
       ]}
     />
   );
